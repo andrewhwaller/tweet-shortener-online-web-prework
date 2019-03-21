d026618d50a1a6829c7a18facd6533867058f158
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 07:01:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,6 +24,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
-  tweets.each {|tweet|
-    puts word_substituter(tweet)}
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
